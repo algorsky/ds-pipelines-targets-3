3bd0a5bffdd8af3f4e2812d8e3a706f54e9bc0db
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro-2.local>
2021-12-20 16:35:15 -0600

    Changed to dynamic branching from static branching.

--- _targets.R
+++ _targets.R
@@ -24,57 +24,50 @@ source("3_visualize/src/map_timeseries.R")
 
 # Configuration
 states <- c('AL','AZ','AR','CA','CO','CT','DE','DC','FL','GA','ID','IL','IN','IA',
-            'KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
-            'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX',
-            'UT','VT','VA','WA','WV','WI','WY','AK','HI','GU','PR')
+           'KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
+           'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX',
+           'UT','VT','VA','WA','WV','WI','WY','AK','HI','GU','PR')
 parameter <- c('00060')
 
-
-# Static Branching for the tar_map
-mapped_by_state_targets<- tar_map(
-    values = tibble(state_abb = states)%>%
-      mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png", state_abb)),
-
-    tar_target(nwis_inventory, filter(oldest_active_sites, state_cd == state_abb)),
-
-    tar_target(nwis_data, retry(get_site_data(nwis_inventory, state_abb, parameter),
-                                when = "Ugh, the internet data transfer failed!",
-                                max_tries = 30)),
-
-    #Insert step for tallying data here
-    tar_target(tally, tally_site_obs(nwis_data)),
-
-    #Insert step for plotting data
-    tar_target(timeseries_png, plot_site_data(state_plot_files,
-                                              nwis_data, parameter),
-               format = "file"),
-    names = state_abb,
-    unlist = FALSE
-  )
 
 #list of Targets
 list(
   # Identify oldest sites
   tar_target(oldest_active_sites, find_oldest_sites(states, parameter)),
 
+  #NWIS Inventory
+  tar_target(nwis_inventory,
+             oldest_active_sites%>%
+               group_by(state_cd)%>%
+               tar_group(),
+             iteration = "group"),
 
-  #Static Branching by state
-  mapped_by_state_targets,
+  #Downloading NWIS data
+  tar_target(nwis_data, retry(get_site_data(nwis_inventory, nwis_inventory$state_cd, parameter),
+                              when = "Ugh, the internet data transfer failed!",
+                              max_tries = 30),
+             pattern = map(nwis_inventory)),
 
-  # Tar combine for the observation tallies
-  tar_combine(obs_tallies, mapped_by_state_targets$tally,
-              command = combine_obs_tallies(!!!.x)),
+  #Insert step for tallying data here
+  tar_target(tally, tally_site_obs(nwis_data), pattern = map(nwis_data)),
 
-  tar_combine(
+  #Insert step for plotting data
+  tar_target(timeseries_png, plot_site_data(
+    sprintf("3_visualize/out/timeseries_%s.png", unique(nwis_data$State)),
+             nwis_data, parameter),
+  format="file",
+  pattern = map(nwis_data)),
+
+  #Summary Timeseries .csv
+  tar_target(
     summary_state_timeseries_csv,
-    mapped_by_state_targets$timeseries_png,
-    command = summarize_targets('3_visualize/log/summary_state_timeseries.csv', !!!.x),
+    command = summarize_targets('3_visualize/log/summary_state_timeseries.csv', names(timeseries_png)),
     format="file"
   ),
 
   #Plot of the data coverage
-  tar_target(obs_tallies_png,
-             plot_data_coverage(obs_tallies,
+  tar_target(data_coverage_png,
+             plot_data_coverage(tally,
                                 "3_visualize/out/data_coverage.png", parameter),
              format = "file"),
 
